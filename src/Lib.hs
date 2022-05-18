cbcdb3cb6495cefd1f1485feffe192e9227bdf8d
daigoro <daigoro.abe@systemi.co.jp>
2022-05-18 17:22:46 +0900

    Modified Lib Module
    数独問題の全探索を行うように Lib モジュールを変更

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,17 +1,35 @@
 module Lib
-    ( someFunc
-    ) where
+  ( someFunc
+  ) where
 
-import Sudoku.Problem
-import System.IO
-import Numeric
+import           Numeric
+import           Sudoku.Problem
+import           Sudoku.Solver
+import           System.IO
 
 someFunc :: IO ()
-someFunc = do 
-    handle <- openFile "resources/sudoku.txt" ReadMode
-    text <- hGetContents handle
-    let problems = extractProblemsFromString $ lines text
+someFunc = do
+  handle <- openFile "resources/sudoku.txt" ReadMode
+  text   <- hGetContents handle
+  let problems = extractProblemsFromString $ lines text
 
-    mapM_ (\p -> do{print $ blockIndex p}) $ head problems
+  mapM_ showProblemAndAnswer problems
 
-    hClose handle
+  hClose handle
+
+showProblemAndAnswer :: SudokuProblem -> IO ()
+-- 問題
+showProblemAndAnswer sp = do
+  mapM_
+      (\x -> do
+        print $ show x
+      )
+    $ (convertToUnits sp rowIndex)
+  putStrLn ""
+-- 答え
+  mapM_
+      (\x -> do
+        print $ show x
+      )
+    $ (convertToUnits (exhausiveSearch sp (head sp)) rowIndex)
+  putStrLn "---------------------"
